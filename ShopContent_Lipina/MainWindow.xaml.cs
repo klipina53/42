70cc6e7398b0367d156ecce36cc116cf5f2708d4
klipina53 <klipina53@gmail.com>
2024-12-11 04:51:02 +0500

    Разработанное главное окно.Lipina

--- ShopContent_Lipina/MainWindow.xaml.cs
+++ ShopContent_Lipina/MainWindow.xaml.cs
@@ -20,9 +20,18 @@ namespace ShopContent_Lipina
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static MainWindow init;
+        public View.Main Main = new View.Main();
+
         public MainWindow()
         {
             InitializeComponent();
+            init = this;
+            frame.Navigate(Main);
+        }
+        private void OpenIndex(object sender, MouseButtonEventArgs e)
+        {
+            frame.Navigate(Main);
         }
     }
 }
