eea80d2bd506d20d319930b489079cea2e132a49
klipina53 <klipina53@gmail.com>
2024-12-11 05:47:15 +0500

    Разработанные контексты данных.Lipina

---
+++ ShopContent_Lipina/Context/CategoriesContext.cs
@@ -0,0 +1,30 @@
+﻿using ShopContent_Lipina.Classes;
+using ShopContent_Lipina.Modell;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace ShopContent_Lipina.Context
+{
+    public class CategoriesContext : Categories
+    {
+        public static ObservableCollection<CategoriesContext> AllCategories()
+        {
+            ObservableCollection<CategoriesContext> allCategories = new ObservableCollection<CategoriesContext>();
+            SqlConnection connection;
+            SqlDataReader dataCategories = Connection.Query("SELECT * FROM [dbo].[Categories]", out connection);
+            while(dataCategories.Read())
+            {
+                allCategories.Add(new CategoriesContext()
+                {
+                    Id = dataCategories.GetInt32(0),
+                    Name = dataCategories.GetString(1)
+                });
+            }
+            Connection.CloseConnection(connection);
+            return allCategories;
+        }
+    }
+}
