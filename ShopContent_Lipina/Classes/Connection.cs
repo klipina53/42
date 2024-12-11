8aedd8f71887d8748b39f376de3ba4182bf9d95d
klipina53 <klipina53@gmail.com>
2024-12-11 05:13:03 +0500

    Разработанные классы общего пользования.Lipina

---
+++ ShopContent_Lipina/Classes/Connection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace ShopContent_Lipina.Classes
+{
+    public class Connection
+    {
+        private static readonly string config = "server=student.permaviat.ru;" +
+            "Trusted_Connection=No;" +
+            "DataBase=base2_ISP_21_2_17;" +
+            "User=ISP_21_2_17;" +
+            "PWD=Tz4ZP5o6W#";
+        public static SqlConnection OpenConnection()
+        {
+            SqlConnection connection = new SqlConnection(config);
+            connection.Open();
+            return connection;
+        }
+        public static SqlDataReader Query(string SQL, out SqlConnection connection)
+        {
+            connection = OpenConnection();
+            return new SqlCommand(SQL, connection).ExecuteReader();
+        }
+        public static void CloseConnection(SqlConnection connection)
+        {
+            connection.Close();
+            SqlConnection.ClearPool(connection);
+        }
+    }
+}
