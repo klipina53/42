8639031c2bb3dcffbaeed0fd79f0e2aa5d2d56c9
klipina53 <klipina53@gmail.com>
2024-12-11 06:05:54 +0500

    Разработанные модели представления.Lipina

---
+++ ShopContent_Lipina/ViewModell/VMCategories.cs
@@ -0,0 +1,24 @@
+﻿using ShopContent_Lipina.Modell;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace ShopContent_Lipina.ViewModell
+{
+    public class VMCategories : INotifyPropertyChanged
+    {
+        public ObservableCollection<Context.CategoriesContext> Categories { get; set; }
+        public VMCategories() =>
+            Categories = Context.CategoriesContext.AllCategories();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
